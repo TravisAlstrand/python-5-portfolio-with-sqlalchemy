550b286fc1ed55a40ea145326b528e81ffd05e3d
Travis Alstrand <travisalstrand@gmail.com>
2022-12-08 13:35:17 -0800

    finishing touches, fixed nav

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def index():
 
 @app.route('/projects/new', methods=['GET', 'POST'])
 def create_project():
+  projects = Project.query.all()
   if request.form:
     new_product = Project(title=request.form['title'],
                           date=data_to_db.clean_date(request.form['date']),
@@ -21,19 +22,21 @@ def create_project():
     db.session.add(new_product)
     db.session.commit()
     return redirect(url_for('index'))
-  return render_template('projectform.html')
+  return render_template('projectform.html', projects=projects)
 
 
 @app.route('/projects/<id>')
 def project_detail(id):
+  projects = Project.query.all()
   project = Project.query.get_or_404(id)
   project.date = project.date.strftime('%m/%d/%Y')
   project.skills = project.skills.split(', ')
-  return render_template('detail.html', project=project)
+  return render_template('detail.html', project=project, projects=projects)
 
 
 @app.route('/projects/<id>/edit', methods=['GET', 'POST'])
 def edit_project(id):
+  projects = Project.query.all()
   project = Project.query.get_or_404(id)
   if request.form:
     project.title = request.form['title']
@@ -44,7 +47,7 @@ def edit_project(id):
     db.session.commit()
     return redirect(url_for('project_detail', id=id))
   project.date = project.date.strftime('%Y-%m-%d')
-  return render_template('editform.html', project=project)
+  return render_template('editform.html', project=project, projects=projects)
 
 
 @app.route('/projects/<id>/delete')
@@ -57,12 +60,14 @@ def delete_project(id):
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  projects = Project.query.all()
+  return render_template('about.html', projects=projects)
 
 
 @app.errorhandler(404)
 def not_found(error):
-  return render_template('404.html', msg=error), 404
+  projects = Project.query.all()
+  return render_template('404.html', msg=error, projects=projects), 404
 
 
 if __name__ == "__main__":
